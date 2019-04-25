aca45400fc8892ceb45de39c3468f18c58faff8d
Steven Goodliff <sgoodliff@gmail.com>
2019-04-25 22:21:00 +0100

    ignore exe and fixed test

--- hello_test.go
+++ hello_test.go
@@ -5,7 +5,7 @@ import "testing"
 // check that hello() returns expected
 func TestHello(t *testing.T) {
 	expected := "Hello Go!"
-	actual := hello()
+	actual := Hello()
 	if actual != expected {
 		t.Error("Test failed")
 	}
